538e6a8f0f3997adfe8792cc2bcc07a6a50e1de1
Nino044 <ninohrgetic65@gmail.com>
2024-06-13 19:14:08 +0200

    Add files via upload

---
+++ Izbornik.c
@@ -0,0 +1,229 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "Header.h"
+
+
+void prikaziGlavniIzbornik() {
+	printf("Odaberite opciju:\n");
+	printf("1. Opcije klijenata\n");
+	printf("2. Opcije vozila\n");
+	printf("3. Opcije popravaka\n");
+	printf("4. Povratak na izbornik administratora\n");
+	printf("0. Izlaz\n");
+	printf("Odabir: ");
+}
+
+void prikaziIzbornikKlijenata() {
+	printf("Odaberite opciju za klijente:\n");
+	printf("1. Unos novog klijenta\n");
+	printf("2. Ispisi klijente\n");
+	printf("3. Sortiraj klijente\n");
+	printf("4. Pretrazi klijenta\n");
+	printf("5. Azuriraj klijenta\n");
+	printf("6. Obrisi klijenta\n");
+	printf("7. Obrisi datoteku klijenata\n");
+	printf("0. Povratak\n");
+	printf("Odabir: ");
+}
+
+void prikaziIzbornikVozila() {
+	printf("Odaberite opciju za vozila:\n");
+	printf("1. Unos novog vozila\n");
+	printf("2. Ispisi vozila\n");
+	printf("3. Povijest vozila\n");
+	printf("4. Sortiraj vozila\n");
+	printf("5. Azuriraj registraciju vozila\n");
+	printf("6. Obrisi vozilo\n");
+	printf("7. Obrisi datoteku vozila\n");
+	printf("0. Povratak\n");
+	printf("Odabir: ");
+}
+
+void prikaziIzbornikPopravaka() {
+	printf("Odaberite opciju za popravke:\n");
+	printf("1. Unos novog popravka\n");
+	printf("2. Ispis popravaka\n");
+	printf("3. Pretrazi popravak po ID-u\n");
+	printf("3. Ispis ukupnih troskova popravaka\n");
+	printf("4. Ukupni trosak popravaka za odredenu godinu\n");
+	printf("5. Sortiraj popravke\n");
+	printf("6. Obrisi datoteku popravaka\n");
+	printf("0. Povratak\n");
+	printf("Odabir: ");
+}
+
+int main() {
+	int izbor;
+	pocetak:
+	do {
+		printf("1. Registracija administratora\n");
+		printf("2. Prijava administratora\n");
+		printf("3. Odjava administratora\n");
+		printf("4. Izlaz\n");
+		printf("Izbor: ");
+
+		if (scanf("%d", &izbor) != 1) {
+			printf("Neispravan unos. Unesite broj.\n");
+			ocistiUlazniSpremnik(); // pozivam funkciju za ciscenje ulaznog spremnika
+			continue;
+		}
+
+		switch (izbor) {
+		case 1:
+			globalnaRegistracijaAdmin();
+			break;
+		case 2:
+			if (globalnaPrijavaAdmin()) {
+				printf("Dobrodosli!\n");
+				goto start;
+			}
+			else {
+				printf("Prijava nije uspjela.\n");
+			}
+			break;
+		case 3:
+			globalnaOdjavaAdmin();
+			goto pocetak;
+		case 4:
+			printf("Izlaz.\n");
+			return 0;
+			break;
+		default:
+			printf("Neispravan izbor. Pokusajte ponovno.\n");
+		}
+	} while (izbor != 3);
+
+	start:
+	do {
+		prikaziGlavniIzbornik();
+		while (scanf("%d", &izbor) != 1) {
+			printf("Neispravan unos. Molimo unesite broj.\n");
+			while (getchar() != '\n'); // cistim ulazni bafer
+			continue;
+		}
+		switch (izbor) {
+		case 1:
+			do {
+				prikaziIzbornikKlijenata();
+				scanf("%d", &izbor);
+
+				switch (izbor) {
+				case 1:
+					unosNovogKlijenta();
+					break;
+				case 2:
+					ispisiKlijente();
+					break;
+				case 3:
+					sortirajKlijente();
+					break;
+				case 4:
+					pretraziIKlijenta();
+					break;
+				case 5:
+					azurirajKlijentaUI();
+					break;
+				case 6:
+					obrisiKlijentaUI();
+					break;
+				case 7:
+					obrisiKlijente();
+					break;
+				case 0:
+					goto start;
+					break;
+				default:
+					printf("Neispravan odabir. Molimo odaberite ponovno.\n");
+				}
+			} while (izbor != 0 && izbor != 1 && izbor != 2 && izbor != 3 && izbor != 4 && izbor != 5 && izbor != 6 && izbor != 7);
+			return 0;
+			break;
+
+		case 2:
+			do {
+				prikaziIzbornikVozila();
+				scanf("%d", &izbor);
+
+				switch (izbor) {
+				case 1:
+					unosNovogVozila();
+					break;
+				case 2:
+					ispisiVozila();
+					break;
+				case 3:
+					povijestVozila();
+					break;
+				case 4:
+					sortirajVozila();
+					break;
+				case 5:
+					azurirajRegistracijuVozila();
+					break;
+				case 6:
+					obrisiVoziloUI();
+					break;
+				case 7:
+					obrisiVozila();
+					break;
+				case 0:
+					goto start;
+					break;
+				default:
+					printf("Neispravan odabir. Molimo odaberite ponovno.\n");
+				}
+			} while (izbor != 0 && izbor != 1 && izbor != 2 && izbor != 3 && izbor != 4 && izbor != 5 && izbor != 6 && izbor != 7);
+			return 0;
+			break;
+
+		case 3:
+			do {
+				prikaziIzbornikPopravaka();
+				scanf("%d", &izbor);
+
+				switch (izbor) {
+				case 1:
+					noviPopravak();
+					break;
+				case 2:
+					ispisPopravaka();
+					break;
+				case 3:
+					ukupniTrosakPopravaka();
+					break;
+				case 4:
+					ukupniTrosakPopravakaZaGodinu();
+					break;
+				case 5:
+					sortirajPopravke();
+					break;
+				case 6:
+					obrisiPopravke();
+					break;
+				case 0:
+					goto start;
+					break;
+				default:
+					printf("Neispravan odabir. Molimo odaberite ponovno.\n");
+				}
+			} while (izbor != 0 && izbor != 1 && izbor != 2 && izbor != 3 && izbor != 4 && izbor != 5 && izbor != 6);
+			return 0;
+			break;
+		case 4:
+			goto pocetak;
+			break;
+		case 0:
+			printf("Izlaz iz programa.\n");
+			return 0;
+			break;
+
+		default:
+			printf("Neispravan odabir. Molimo odaberite ponovno.\n");
+		}
+	} while (izbor != 0);
+
+	return 0;
+}
---
+++ Vozila.c
@@ -0,0 +1,341 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Header.h"
+
+// Funkcija koja provjerava postoji li vec uneseni ID
+int postojiID(int id) {
+	FILE* file = fopen("vozila.txt", "r+");
+	if (file == NULL) {
+		perror("Ne moze se otvoriti datoteka za vozila");
+		exit(1);
+	}
+
+	int postojeciId;
+	char marka[MADE];
+	char model[MODEL];
+	char registracija[REGIST];
+	int godinaProizvodnje;
+
+	while (fscanf(file, "%d %49s %49s %14s %d", &postojeciId, marka, model, registracija, &godinaProizvodnje) == 5) {
+		if (postojeciId == id) {
+			fclose(file);
+			return 1;  // ID postoji
+		}
+	}
+
+	fclose(file);
+	return 0;  // ID ne postoji
+}
+
+void unosNovogVozila() {
+	
+	int id;
+	char marka[MADE];
+	char model[MODEL];
+	char registracija[REGIST];
+	int godinaProizvodnje;
+
+	printf("Unesite ID: ");
+	if (scanf("%d", &id) != 1) {
+		printf("Neispravan unos ID-a.\n");
+		return;
+	}
+
+	// Provjera postoji li ID
+	if (postojiID(id)) {
+		printf("Vozilo s tim ID-om vec postoji.\n");
+		return;
+	}
+
+	printf("Unesite marku: ");
+	if (scanf("%s", marka) != 1) {
+		printf("Neispravan unos marke.\n");
+		return;
+	}
+	printf("Unesite model: ");
+	if (scanf("%s", model) != 1) {
+		printf("Neispravan unos modela.\n");
+		return;
+	}
+	printf("Unesite registraciju: ");
+	if (scanf("%s", registracija) != 1) {
+		printf("Neispravan unos registracije.\n");
+		return;
+	}
+	printf("Unesite godinu proizvodnje: ");
+	if (scanf("%d", &godinaProizvodnje) != 1) {
+		printf("Neispravan unos godine proizvodnje.\n");
+		return;
+	}
+
+	FILE* file = fopen("vozila.txt", "a");
+	if (file == NULL) {
+		perror("Ne moze se otvoriti datoteka za vozila");
+		exit(1);
+	}
+
+	fprintf(file, "%d %s %s %s %d\n", id, marka, model, registracija, godinaProizvodnje);
+	fclose(file);
+
+	printf("Vozilo je uspjesno dodano.\n");
+}
+
+void ispisiVozilaHelper(FILE* file) {
+	Vozilo vozilo;
+
+	if (fscanf(file, "%d %49s %49s %14s %d", &vozilo.id, vozilo.marka, vozilo.model, vozilo.registracija, &vozilo.godinaProizvodnje) == 5) {
+		printf("| %-2d | %-28s | %-28s | %-13s | %-18d |\n", vozilo.id, vozilo.marka, vozilo.model, vozilo.registracija, vozilo.godinaProizvodnje);
+		printf("+----+------------------------------+------------------------------+---------------+--------------------+\n");
+		ispisiVozilaHelper(file);
+	}
+}
+
+void ispisiVozila() {
+	FILE* file = fopen("vozila.txt", "r");
+	if (file == NULL) {
+		perror("Ne moze se otvoriti datoteka za vozila");
+		exit(1);
+	}
+
+	printf("+----+------------------------------+------------------------------+---------------+--------------------+\n");
+	printf("| ID |           Marka              |            Model             | Registracija  | Godina proizvodnje |\n");
+	printf("+----+------------------------------+------------------------------+---------------+--------------------+\n");
+
+	// postavljam citac na pocetak datoteke
+	rewind(file);
+
+	// Rekurzivno pozivamo pomocnu funkciju za ispisivanje podataka iz datoteke
+	ispisiVozilaHelper(file);
+
+	fclose(file);
+
+}
+
+
+
+void povijestVozila() {
+	FILE* file = fopen("popravci.txt", "r");
+	if (file == NULL) {
+		perror("Ne moze se otvoriti datoteka za citanje popravaka");
+		return;
+	}
+
+	int idVozila;
+	printf("\nUnesite ID vozila za koje zelite vidjeti povijest: ");
+	if (scanf("%d", &idVozila) != 1) {
+		printf("Neispravan unos ID-a vozila.\n");
+		fclose(file);
+		return;
+	}
+
+	Popravak popravak;
+
+	printf("\nPovijest popravaka za vozilo s ID %d:\n", idVozila);
+	printf("-----------------------------------------------------------------------------------------------------------------------\n");
+
+	while (fscanf(file, "%d %d %d %254[^0-9] %lf %10s %254[^\n]", &popravak.id, &popravak.idVozila, &popravak.idKlijenta, popravak.opis, &popravak.cijena, popravak.datum, popravak.dijelovi) == 7) {
+		if (popravak.idVozila == idVozila) {
+
+			printf("ID popravka: %d\n", popravak.id);
+			printf("ID klijenta: %d\n", popravak.idKlijenta);
+			printf("Opis: %s\n", popravak.opis);
+			printf("Cijena: %.2f EUR\n", popravak.cijena);
+			printf("Datum: %s\n", popravak.datum);
+			printf("Koristeni dijelovi: %s\n", popravak.dijelovi);
+			printf("-----------------------------------------------------------------------------------------------------------------------\n");
+		}
+	}
+
+	fclose(file);
+}
+
+int usporediVozila(const void* a, const void* b) {
+	const Vozilo* voziloA = (const Vozilo*)a;
+	const Vozilo* voziloB = (const Vozilo*)b;
+	return strcmp(voziloA->marka, voziloB->marka);
+}
+
+void sortirajVozila() {
+	FILE* file = fopen("vozila.txt", "r");
+	if (file == NULL) {
+		perror("Ne moze se otvoriti datoteka za vozila");
+		return;
+	}
+
+	Vozilo* vozila = NULL;
+	int brojVozila = 0;
+	Vozilo tempVozilo;
+
+	while (fscanf(file, "%d %19s %49s %14s %d", &tempVozilo.id, tempVozilo.marka, tempVozilo.model, tempVozilo.registracija, &tempVozilo.godinaProizvodnje) == 5) {
+		vozila = realloc(vozila, sizeof(Vozilo) * (brojVozila + 1));
+		if (vozila == NULL) {
+			perror("Ne moze se alocirati memorija za vozila");
+			fclose(file);
+			return;
+		}
+		vozila[brojVozila++] = tempVozilo;
+	}
+	fclose(file);
+
+	qsort(vozila, brojVozila, sizeof(Vozilo), usporediVozila);
+
+	file = fopen("vozila.txt", "w");
+	if (file == NULL) {
+		perror("Ne moze se otvoriti datoteka za pisanje vozila");
+		free(vozila);
+		return;
+	}
+
+	for (int i = 0; i < brojVozila; i++) {
+		fprintf(file, "%d %s %s %s %d\n", vozila[i].id, vozila[i].marka, vozila[i].model, vozila[i].registracija, vozila[i].godinaProizvodnje);
+	}
+	fclose(file);
+	free(vozila);
+
+	printf("\nVozila su uspjesno sortirana.\n");
+}
+
+void azurirajRegistracijuVozila() {
+	FILE* file = fopen("vozila.txt", "r");
+	if (file == NULL) {
+		perror("Ne moze se otvoriti datoteka za citanje vozila");
+		return;
+	}
+
+	Vozilo* vozila = NULL;
+	int brojVozila = 0;
+	Vozilo tempVozilo;
+
+	while (fscanf(file, "%d %49s %49s %14s %d", &tempVozilo.id, tempVozilo.marka, tempVozilo.model, tempVozilo.registracija, &tempVozilo.godinaProizvodnje) == 5) {
+		vozila = realloc(vozila, sizeof(Vozilo) * (brojVozila + 1));
+		if (vozila == NULL) {
+			perror("Neuspjesno zauzimanje memorije za vozila");
+			fclose(file);
+			return;
+		}
+		vozila[brojVozila++] = tempVozilo;
+	}
+	fclose(file);
+
+	int idVozila;
+	printf("Unesite ID vozila koje zelite azurirati: ");
+	if (scanf("%d", &idVozila) != 1) {
+		printf("Neispravan unos ID-a vozila.\n");
+		free(vozila);
+		return;
+	}
+
+	int found = 0;
+	for (int i = 0; i < brojVozila; ++i) {
+		if (vozila[i].id == idVozila) {
+			found = 1;
+			printf("Unesite novu registraciju: ");
+			if (scanf("%s", vozila[i].registracija) != 1) {
+				printf("Neispravan unos registracije.\n");
+				free(vozila);
+				return;
+			}
+			break;
+		}
+	}
+
+	if (!found) {
+		printf("Vozilo s ID %d nije pronadeno.\n", idVozila);
+		free(vozila);
+		return;
+	}
+
+	file = fopen("vozila.txt", "w");
+	if (file == NULL) {
+		perror("Ne moze se otvoriti datoteka za pisanje vozila");
+		free(vozila);
+		return;
+	}
+
+	for (int i = 0; i < brojVozila; ++i) {
+		fprintf(file, "%d %s %s %s %d\n", vozila[i].id, vozila[i].marka, vozila[i].model, vozila[i].registracija, vozila[i].godinaProizvodnje);
+	}
+
+	fclose(file);
+	free(vozila);
+
+	printf("Registracija vozila s ID %d uspjesno azurirana.\n", idVozila);
+}
+
+void obrisiVozila() {
+
+	const char* imeDatoteke = "vozila.txt";
+
+	if (remove(imeDatoteke) == 0) {
+		printf("Datoteka 'vozila.txt' je uspjesno obrisana.\n");
+	}
+	else {
+		perror("Greska pri brisanju datoteke");
+	}
+}
+
+void obrisiVozilo(int id) {
+	FILE* file = fopen("vozila.txt", "r");
+	if (file == NULL) {
+		perror("Ne može se otvoriti datoteka za èitanje vozila");
+		return;
+	}
+
+	Vozilo* vozila = NULL;
+	int brojVozila = 0;
+	Vozilo tempVozilo;
+
+	while (fscanf(file, "%d %49s %49s %14s %d", &tempVozilo.id, tempVozilo.marka, tempVozilo.model, tempVozilo.registracija, &tempVozilo.godinaProizvodnje) == 5) {
+		vozila = realloc(vozila, sizeof(Vozilo) * (brojVozila + 1));
+		if (vozila == NULL) {
+			perror("Ne može se alocirati memorija za vozila");
+			fclose(file);
+			return;
+		}
+		vozila[brojVozila++] = tempVozilo;
+	}
+	fclose(file);
+
+	int found = 0;
+	for (int i = 0; i < brojVozila; i++) {
+		if (vozila[i].id == id) {
+			for (int j = i; j < brojVozila - 1; j++) {
+				vozila[j] = vozila[j + 1];
+			}
+			brojVozila--;
+			found = 1;
+			break;
+		}
+	}
+
+	if (!found) {
+		printf("Vozilo s ID %d nije pronadeno.\n", id);
+		free(vozila);
+		return;
+	}
+
+	file = fopen("vozila.txt", "w");
+	if (file == NULL) {
+		perror("Ne može se otvoriti datoteka za pisanje vozila");
+		free(vozila);
+		return;
+	}
+
+	for (int i = 0; i < brojVozila; i++) {
+		fprintf(file, "%d %s %s %s %d\n", vozila[i].id, vozila[i].marka, vozila[i].model, vozila[i].registracija, vozila[i].godinaProizvodnje);
+	}
+	fclose(file);
+	free(vozila);
+
+	printf("Vozilo s ID %d je uspjesno obrisano.\n", id);
+}
+
+void obrisiVoziloUI() {
+	int id;
+	printf("Unesite ID vozila za brisanje: ");
+	scanf("%d", &id);
+	obrisiVozilo(id);
+}
